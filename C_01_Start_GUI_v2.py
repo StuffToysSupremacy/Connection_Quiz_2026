aba150c7d1f61b089b2bb54c49e08b85c2137c5b
StuffToysSupremacy <159388907+StuffToysSupremacy@users.noreply.github.com>
2026-05-01 09:01:15 +1200

    Start GUI, checks how many question

---
+++ C_01_Start_GUI_v2.py
@@ -0,0 +1,122 @@
+from tkinter import *
+
+class StartQuiz():
+    """
+    Initial Game inter-face (asks users how many questions they would like to do)
+    """
+
+    def __init__(self):
+        """
+        Gets number of questions from user
+        """
+        background = "#ffe6cc"
+        self.start_frame = Frame(padx=10, pady=10, bg="#E1D5E7")
+        self.start_frame.grid()
+
+        # Strings for labels
+        intro_string = ("In each question you will be asked to choose between four "
+                        "options. Your goal is to select the right option of which word "
+                        "connects with the word given. \n\n "
+                        "To begin, please decide how many questions you want to do.")
+
+        #  choose_string = "Oops - Please choose a whole number more than zero"
+        choose_string = "How many questions would you like to do?"
+
+        # List of labels to be made (text | font | fg | bg)
+        start_labels_list = [
+            ["Connection Quiz", ("Arial", 16, "bold"), None],
+            [intro_string, ("Arial", 12), None ],
+            [choose_string, ("Arial", 12, "bold"), "#009900" ]
+        ]
+
+        # create labels and add them to the reference list...
+
+        start_label_ref = []
+        for count, item in enumerate(start_labels_list):
+            make_label = Label(self.start_frame, text=item[0],font=item[1],
+                               fg=item[2], bg="#E1D5E7",
+                               wraplength=350, justify="left", pady=10,padx=20)
+            make_label.grid(row=count)
+
+            start_label_ref.append(make_label)
+
+
+        # extract choice label so that it can be change to an
+        # error message if necessary.
+
+        self.choose_label = start_label_ref[2]
+
+        #  Frame so that entry box and button can be in the same row.
+        self.entry_area_frame = Frame(self.start_frame, bg="#E1D5E7")
+        self.entry_area_frame.grid(row=3)
+
+        self.num_question_entry = Entry(self.entry_area_frame, font=("Arial",20, "bold"),
+                                      width=26,justify='center')
+        self.num_question_entry.grid(row=0, column=0, padx=10, pady=10)
+
+        # Create start button...
+        self.start_button = Button(self.entry_area_frame, font=("Arial", 16, "bold"),
+                                  fg="#FFFFFF", bg="#5555D4", text="Start", width=30,
+                                  command=self.check_questions)
+
+        self.start_button.grid(row=1)
+
+    def check_questions(self):
+        """
+         Checks users have entered 1 or more question
+        """
+
+        # Retrieve how many questions wanted
+        questions_wanted = self.num_question_entry.get()
+
+        # Reset label and entry box (for when users come back to home screen)
+        self.choose_label.config(fg="#009900", font=("Arial", 12, "bold"))
+        self.num_question_entry.config(bg="#FFFFFF")
+
+        error = "Oops - Please choose a whole number more than zero."
+        has_errors = "no"
+
+        # checks that # to do is a number above absolute zero
+        try:
+            questions_wanted = int(questions_wanted)
+            if questions_wanted > 0:
+                # Invoke Test Class (and take across number of rounds)
+                Test(questions_wanted)
+                # Hide root window (ie: hide question choice window)
+                root.withdraw()
+            else:
+                has_errors = "yes"
+
+        except ValueError:
+            has_errors = "yes"
+
+        # display the error if necessary
+        if has_errors == "yes":
+            self.choose_label.config(text=error, fg="#990000",
+                                     font=("Arial", 10, "bold"))
+            self.num_question_entry.config(bg="#F4CCCC")
+            self.num_question_entry.delete(0, END)
+
+
+class Test:
+    """
+    Interface for answering the Connection Quiz
+    """
+
+    def __init__(self, how_many):
+        self.test_box = Toplevel()
+
+        self.quiz_frame = Frame(self.test_box)
+        self.quiz_frame.grid(padx=10, pady=10)
+
+        self.test_heading_label = Label(self.test_box, text=f"Question 0 of {how_many}",
+                                        font=("Arial", 16, "bold"), bg="#C09CD2")
+
+        self.test_heading_label.grid(row=0)
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Connection Quiz")
+    StartQuiz()
+    root.mainloop()
